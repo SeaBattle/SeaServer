b072232cf9e9ac69bb65218ee067791834cf64fd
Tihon <com.tihon@mail.ru>
2016-09-16 11:37:57 +0300

    refactor of firing logics

--- src/logics/game/ss_game_logic.erl
+++ src/logics/game/ss_game_logic.erl
@@ -47,15 +47,17 @@ set_fleet(_, _, State) -> %fleet package was got from unknown player. May be wro
   {prepare, {error, ?WRONG_GAME}, State}.
 
 %% Fire (or another action, which takes player's turn). Update map, change turn if needed,
-%% update players ships. If no ships left - end game.
+%% update players ships. If no ships left - end game. %TODO make game_state use map for fleet and ships of players to avoid code duplication
 fire(Shot = #{?FIRE_TYPE_HEAD := X, ?SHOT_HEAD := Shots}, Pid, State = #game_state{shots_left = M}) when ?NORMAL(X), length(Shots) =:= M ->
   case State of
     #game_state{player1 = {Pid, _}, player2 = {Pid2, _}, active = 0} -> %turn of player1 and he makes it
       #game_state{fleet_player2 = Map2, ships_player2 = Ships2} = State,
-      do_fire(Shot, Shots, Map2, Ships2, Pid2, State);
+      {NextState, Result, UMap, UShips, UState} = do_fire(Shot, Shots, Map2, Ships2, Pid2, State),
+      {NextState, Result, UState#game_state{fleet_player2 = UMap, ships_player2 = UShips}};
     #game_state{player2 = {Pid, _}, player1 = {Pid1, _}, active = 1} -> %turn of player2 and he makes it
       #game_state{fleet_player1 = Map1, ships_player1 = Ships1} = State,
-      do_fire(Shot, Shots, Map1, Ships1, Pid1, State);
+      {NextState, Result, UMap, UShips, UState} = do_fire(Shot, Shots, Map1, Ships1, Pid1, State),
+      {NextState, Result, UState#game_state{fleet_player1 = UMap, ships_player1 = UShips}};
     _ ->
       {play, {error, ?WRONG_TURN}, State}
   end;
@@ -66,85 +68,82 @@ fire(Fire, Pid, State) ->
   end.
 
 %% @private
-do_fire(Shot, Shots, Map, Ships, Pid, State) ->
-  notify_player(Pid, {fire, Shot}),
-  case do_fire_cycle(Shots, Map, Ships, State) of
-    A = {end_game, _} -> A;
-    {[], UState} -> {play, [], UState};
-    {Hits, UState} -> {play, Hits, UState}
+do_fire(Shot, Shots, Map, Ships, Enemy, State) -> %TODO proper return format for players
+  case do_fire_cycle(Shots, Map, Ships) of
+    {Hits, UMap, []} -> %all ships ended - end game
+      notify_player(Enemy, {fire, Shot, loose}),
+      {end_game, {win, Hits}, UMap, [], State};
+    {Hits, UMap, UShips} ->
+      {Change, UState} = change_turn(State, get_hits_num(UShips)),
+      notify_player(Enemy, {fire, Shot, {change, Change}}),
+      {play, {Hits, {change, Change}}, UMap, UShips, UState}
   end.
 
 %% @private
-do_fire_cycle(Shots, Map, Ships, State) ->
+do_fire_cycle(Shots, Map, Ships) ->
   lists:foldl(
-    fun
-      (_, {end_game, UState}) -> {end_game, UState};
-      (#{?FIRE_X_HEAD := X, ?FIRE_Y_HEAD := Y}, {Hits, UState = #game_state{shots_left = M}}) ->
-        do_one_shot(Map, X, Y, Ships, UState#game_state{shots_left = M - 1}, Hits)
-    end, {[], State}, Shots).
+    fun(#{?FIRE_X_HEAD := X, ?FIRE_Y_HEAD := Y}, {Hits, UMap, UShips}) ->
+      do_one_shot(UMap, X, Y, UShips, Hits)
+    end, {[], Map, Ships}, Shots).
 
 %% @private
-do_one_shot(Map, X, Y, Ships, State = #game_state{player1 = {P1, _}, player2 = {P2, _}, active = N, rules = Rules}, Hits) ->
+do_one_shot(Map, X, Y, Ships, Hits) ->
   case mark_fire(Map, X, Y, Ships) of
-    {true, [], UMap2} ->   %ship was hit (no more ships for player)
-      notify_active_player(P1, P2, N, win),
-      notify_active_player(P1, P2, 1 - N, loose),
-      {end_game, State#game_state{fleet_player2 = UMap2, ships_player2 = []}};
-    {true, UShips2, UMap2} ->   %ship was hit
-      N = get_repeat_shot(X, Y, Rules),
-      UHits = N ++ Hits,
-      UState = should_change_turn(State, UHits),
-      {UHits, UState#game_state{fleet_player2 = UMap2, ships_player2 = UShips2}};
-    {false, UMap2} ->  %miss, change turn
-      UState = should_change_turn(State, Hits),
-      {Hits, UState#game_state{fleet_player2 = UMap2}}
+    {{kill, Id, []}, UMap2} ->   %ship was killed (no more ships for player) -> end game
+      {[{X, Y, kill, Id} | Hits], UMap2, []};
+    {{Hit, Id, UShips2}, UMap2} when Hit =:= kill; Hit =:= hit ->   %ship was hit or killed
+      {[{X, Y, Hit, Id} | Hits], UMap2, UShips2};
+    {{miss, _}, UMap2} ->  %miss, change turn
+      {[{X, Y, miss} | Hits], UMap2, Ships}
   end.
 
 %% @private
-get_repeat_shot(X, Y, #{?REPEAT_ON_HIT_HEAD := true}) -> [{X, Y}];
-get_repeat_shot(_, _, #{?REPEAT_ON_HIT_HEAD := false}) -> [].
+get_hits_num(Shots) ->
+  length(lists:filter(
+    fun
+      ({_, _, miss}) -> false;
+      (_) -> true
+    end, Shots)).
 
 %% @private
--spec should_change_turn(#game_state{}, list()) -> {integer(), #game_state{}}.
-should_change_turn(State = #game_state{shots_left = 0, rules = #{?FIRES_PER_TURN_HEAD := M}}, []) ->   %no hits, shots ended. Change turn
-  #game_state{player1 = {P1, _}, player2 = {P2, _}, active = N} = State,
-  notify_turn(P1, P2, 1 - N),
-  State#game_state{active = 1 - N, shots_left = M};
-should_change_turn(State = #game_state{rules = Rules, shots_left = 0}, Hits) when length(Hits) > 0 ->   %was some hits, shots ended. Should change turn depends on rules
-  #game_state{player1 = {P1, _}, player2 = {P2, _}, active = N} = State,
+-spec change_turn(#game_state{}, integer()) -> {boolean(), #game_state{}}.
+change_turn(State = #game_state{rules = #{?FIRES_PER_TURN_HEAD := M}, active = N}, 0) ->   %no hits. Change turn
+  {true, State#game_state{active = 1 - N, shots_left = M}};
+change_turn(State = #game_state{rules = Rules, active = N}, K) when K > 0 ->   %was some hits. Should change turn depends on rules
   case Rules of
     #{?REPEAT_ON_HIT_HEAD := true} -> %repeat turn on hit
-      notify_turn(P1, P2, N),
-      State#game_state{shots_left = length(Hits)};
+      {false, State#game_state{shots_left = K}};
     #{?REPEAT_ON_HIT_HEAD := false, ?FIRES_PER_TURN_HEAD := M} ->  %no repeat on hit - change turn
-      notify_turn(P1, P2, 1 - N),
-      State#game_state{active = 1 - N, shots_left = M}
-  end;
-should_change_turn(State, _) ->   %hit or miss - do not change the turn, other shots left (case of multishot)
-  State.
-
-%% @private
-mark_fire(Map, X, Y, Ships) ->
-  case ss_map_logic:perform_fire(Map, X, Y) of
-    {true, Id, UMap2} ->
-      {true, fire_at_ship(Ships, Id), UMap2};
-    {false, UMap2} ->
-      {false, Ships, UMap2}
+      {true, State#game_state{active = 1 - N, shots_left = M}}
   end.
 
 %% @private
+-spec mark_fire(map(), pos_integer(), pos_integer(), list()) ->
+  {{kill | hit, pos_integer(), list()} | {miss, list()}, map()}.
+mark_fire(Map, X, Y, Ships) ->
+  case ss_map_logic:perform_fire(Map, X, Y) of
+    {true, Id, UMap} ->
+      {fire_at_ship(Ships, Id), UMap};
+    {false, UMap} ->
+      {{miss, Ships}, UMap}
+  end.
+
+%% @private
+-spec fire_at_ship(list(), pos_integer()) -> {kill | hit, pos_integer(), list()}.
 fire_at_ship(Ships, N) ->
   case lists:keyfind(N, 1, Ships) of
-    {N, 1} -> lists:keydelete(N, 1, Ships);
-    {N, Size} -> lists:keyreplace(N, 1, Ships, {N, Size - 1})
+    {N, 1} -> {kill, N, lists:keydelete(N, 1, Ships)};
+    {N, Size} -> {hit, N, lists:keyreplace(N, 1, Ships, {N, Size - 1})}
   end.
 
 %% @private
 start_game(#game_state{player1 = {P1, _}, player2 = {P2, _}, active = ActiveNum}) ->
   notify_player(P1, start),
   notify_player(P2, start),
-  notify_active_player(P1, P2, ActiveNum, turn),
-  notify_active_player(P1, P2, 1 - ActiveNum, enemy_turn).
+  case ActiveNum of %TODO change this, when go to map game_state
+    0 -> notify_turn(P1, P2);
+    1 -> notify_turn(P2, P1)
+  end.
 
 %% @private
 form_ships(Ships) ->
@@ -154,13 +153,9 @@ form_ships(Ships) ->
     end, [], Ships).
 
 %% @private
-notify_turn(P1, P2, N) ->
-  notify_active_player(P1, P2, 1 - N, turn),
-  notify_active_player(P1, P2, N, enemy_turn).
-
-%% @private
-notify_active_player(Pid1, _, 0, Notification) -> notify_player(Pid1, Notification);
-notify_active_player(_, Pid2, 1, Notification) -> notify_player(Pid2, Notification).
+notify_turn(P1, P2) ->
+  notify_player(P1, turn),
+  notify_player(P2, enemy_turn).
 
 %% @private %TODO send messages format to players?
 notify_player(Pid, Notification) -> Pid ! Notification.
